ac3219de869993d89b2571132d1de7b291e2d52c
Akila Prabath <akilaprabath861@gmail.com>
2024-12-29 18:29:38 +0530

    created - remove product from wishlist

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,7 @@ use App\Http\Controllers\WishlistController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
-
-
+use Laravel\Jetstream\Role;
 
 /*Route::middleware([
     'auth:sanctum',
@@ -42,6 +40,8 @@ Route::delete('/cart/clear', [CartController::class, 'empty_cart'])->name('cart.
 
 Route::post('/wishlist/add',[WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
 Route::get('/wishlist',[WishlistController::class, 'index'])->name('wishlist.index');
+Route::delete('/wishlist/item/remove/{rowId}',[WishlistController::class, 'remove_item'])->name('wishlist.item.remove');
+Route::delete('wishlist/clear',[WishlistController::class, 'empty_wishlist'])->name('wishlist.items.clear');
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
